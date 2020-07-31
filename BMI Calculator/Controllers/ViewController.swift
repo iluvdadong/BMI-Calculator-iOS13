3db577a55302aec9f5b832bf04ddc56160f0eb97
Dahae Kim <iluvdadong@naver.com>
2020-07-31 21:20:50 +0900

    Using the UISliders to Calculate the BMI

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightValue: UILabel!
+    @IBOutlet weak var weightValue: UILabel!
+    
+    @IBOutlet weak var heightSliderValue: UISlider!
+    @IBOutlet weak var weightSliderValue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // print(String(format: "%.3f", totalWorkTimeInHours))
 
+    @IBAction func heightChanged(_ sender: UISlider) {
+        
+        heightValue.text = String(format: "%.1f", (sender.value)) + "m"
+    }
+    
+    @IBAction func weightChanged(_ sender: UISlider) {
+        weightValue.text = String(format: "%.0f", (sender.value)) + "kg"
 
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        var height = heightSliderValue.value
+        var weight = weightSliderValue.value
+        
+        var BMI = weight / powf(height, 2)
+        print(BMI)
+        
+    }
 }
 
